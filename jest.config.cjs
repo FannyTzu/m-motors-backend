7d4077c0c95d381276da58d8a5180e612f0ae46d
FannyTzu <fannytzu@gmail.com>
2026-02-13 11:41:54 +0100

    feat: migrate Jest configuration to CommonJS format and remove old files

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,19 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+      tsconfig: {
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      }
+    }],
+  },
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.ts',
@@ -13,9 +26,6 @@ module.exports = {
     '**/*.spec.ts'
   ],
   moduleFileExtensions: ['ts', 'js', 'json'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
   verbose: true,
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
 };
